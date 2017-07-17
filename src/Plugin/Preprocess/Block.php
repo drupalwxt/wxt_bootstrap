38a5bfc0cf402d4b643ea67a0fa0908eee5d0a10
ptsimard <ptsimard@1972814.no-reply.drupal.org>
2017-07-17 17:07:29 -0400

    Issue #2894646 by ptsimard: Restrict branding-specific block preprocess to the branding block

--- src/Plugin/Preprocess/Block.php
+++ src/Plugin/Preprocess/Block.php
@@ -17,10 +17,6 @@ class Block extends PreprocessBase {
    * {@inheritdoc}
    */
   public function preprocess(array &$variables, $hook, array $info) {
-    /** @var \Drupal\wxt_library\LibraryService $wxt */
-    $wxt = \Drupal::service('wxt_library.service_wxt');
-    $wxt_active = $wxt->getLibraryName();
-    $library_path = $wxt->getLibraryPath();
 
     // Language Handling.
     $language = \Drupal::languageManager()->getCurrentLanguage()->getId();
@@ -28,24 +24,30 @@ class Block extends PreprocessBase {
     $variables['language'] = $language;
     $variables['language_prefix'] = $language_prefix[$language];
 
-    if ($wxt_active == 'ogpl') {
-      $variables['logo'] = $library_path . '/assets/logo.png';
-    }
-    elseif ($wxt_active == 'gc_intranet') {
-      $variables['logo_sttl_svg'] = $library_path . '/assets/wmms-intra.svg';
-    }
-    elseif ($wxt_active == 'gcwu_fegc') {
-      $variables['logo_sttl_svg'] = $library_path . '/assets/wmms.svg';
-      $variables['logo_sig_svg'] = $library_path . '/assets/sig-' . $language . '.svg';
-    }
-    elseif ($wxt_active == 'gcweb') {
-      $variables['logo'] = $library_path . '/assets/sig-blk-' . $language . '.png';
-      $variables['logo_svg'] = $library_path . '/assets/sig-blk-' . $language . '.svg';
-      $variables['logo_bottom_svg'] = $library_path . '/assets/wmms-blk' . '.png';
-      $variables['logo_bottom_svg'] = $library_path . '/assets/wmms-blk' . '.svg';
-    }
-    elseif ($wxt_active == 'gc_intranet') {
-      $variables['logo_svg'] = $library_path . '/assets/sig-blk-' . $language . '.svg';
+    // Customize branding per WxT theme.
+    if (isset($variables['plugin_id']) && $variables['plugin_id'] == 'system_branding_block') {
+      /** @var \Drupal\wxt_library\LibraryService $wxt */
+      $wxt = \Drupal::service('wxt_library.service_wxt');
+      $wxt_active = $wxt->getLibraryName();
+      $library_path = $wxt->getLibraryPath();
+
+      if ($wxt_active == 'ogpl') {
+        $variables['logo'] = $library_path . '/assets/logo.png';
+      }
+      elseif ($wxt_active == 'gc_intranet') {
+        $variables['logo_sttl_svg'] = $library_path . '/assets/wmms-intra.svg';
+      }
+      elseif ($wxt_active == 'gcwu_fegc') {
+        $variables['logo_sttl_svg'] = $library_path . '/assets/wmms.svg';
+        $variables['logo_sig_svg'] = $library_path . '/assets/sig-' . $language . '.svg';
+      }
+      elseif ($wxt_active == 'gcweb') {
+        $variables['logo'] = $library_path . '/assets/sig-blk-' . $language . '.png';
+        $variables['logo_svg'] = $library_path . '/assets/sig-blk-' . $language . '.svg';
+      }
+      elseif ($wxt_active == 'gc_intranet') {
+        $variables['logo_svg'] = $library_path . '/assets/sig-blk-' . $language . '.svg';
+      }
     }
 
     parent::preprocess($variables, $hook, $info);
