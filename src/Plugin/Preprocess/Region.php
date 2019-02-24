3b310cb2c6880b65748458fecdb3e96c6e222b6c
William Hearn <sylus1984@gmail.com>
2019-02-24 16:57:48 -0500

    feat(homepage): Add homepage migration + templates

---
+++ src/Plugin/Preprocess/Region.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\wxt_bootstrap\Plugin\Preprocess;
+
+use Drupal\bootstrap\Bootstrap;
+use Drupal\bootstrap\Plugin\Preprocess\PreprocessBase;
+use Drupal\bootstrap\Utility\Variables;
+
+/**
+ * Pre-processes variables for the "region" theme hook.
+ *
+ * @ingroup plugins_preprocess
+ *
+ * @BootstrapPreprocess("region")
+ */
+class Region extends PreprocessBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function preprocessVariables(Variables $variables) {
+    $region = $variables['elements']['#region'];
+    $variables['region'] = $region;
+    $variables['content'] = $variables['elements']['#children'];
+
+    if ($region === 'content_footer' && !empty($variables['content'])) {
+      $variables->addClass(['pagedetails']);
+      if (isset($variables['is_front'])) {
+        $variables->addClass(['container']);
+      }
+    }
+
+  }
+
+}
