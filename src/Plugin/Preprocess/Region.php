03f17f3b03425d2c31b1986d4af4e006da6b3079
William Hearn <sylus1984@gmail.com>
2019-03-31 18:46:36 -0400

    feat(bootstrap): Container class only for gcweb v5

--- src/Plugin/Preprocess/Region.php
+++ src/Plugin/Preprocess/Region.php
@@ -18,6 +18,11 @@ class Region extends PreprocessBase {
    * {@inheritdoc}
    */
   public function preprocessVariables(Variables $variables) {
+
+    /** @var \Drupal\wxt_library\LibraryService $wxt */
+    $wxt = \Drupal::service('wxt_library.service_wxt');
+    $wxt_active = $wxt->getLibraryName();
+
     $region = $variables['elements']['#region'];
     $variables['region'] = $region;
     $variables['content'] = $variables['elements']['#children'];
@@ -26,10 +31,12 @@ class Region extends PreprocessBase {
       $variables->addClass(['container']);
     }
 
-    if ($region === 'content_footer' && !empty($variables['content'])) {
-      $variables->addClass(['pagedetails']);
-      if (!empty($variables['is_front'])) {
-        $variables->addClass(['container']);
+    if ($wxt_active == 'gcweb') {
+      if ($region === 'content_footer' && !empty($variables['content'])) {
+        $variables->addClass(['pagedetails']);
+        if (!empty($variables['is_front'])) {
+          $variables->addClass(['container']);
+        }
       }
     }
   }
