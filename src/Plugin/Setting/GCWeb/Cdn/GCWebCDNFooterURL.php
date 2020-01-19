be8cfe758b3f1d4f22c6495280407fab6cf1721f
git <git@3561502.no-reply.drupal.org>
2020-01-19 00:07:01 -0500

    Issue #3106928 by TJPope: wxt bootstrap linting phpcs error in GCWebCDNFooterURL.php

--- src/Plugin/Setting/GCWeb/Cdn/GCWebCDNFooterURL.php
+++ src/Plugin/Setting/GCWeb/Cdn/GCWebCDNFooterURL.php
@@ -3,8 +3,6 @@
 namespace Drupal\wxt_bootstrap\Plugin\Setting\GCWeb\Cdn;
 
 use Drupal\bootstrap\Plugin\Setting\SettingBase;
-use Drupal\bootstrap\Utility\Element;
-use Drupal\Core\Form\FormStateInterface;
 
 /**
  * The "wxt_gcweb_cdn_footer_url" theme setting.
@@ -23,13 +21,4 @@ use Drupal\Core\Form\FormStateInterface;
  *   }
  * )
  */
-class GCWebCDNFooterURL extends SettingBase {
-
-  /**
-   * {@inheritdoc}
-   */
-  public function alterFormElement(Element $form, FormStateInterface $form_state, $form_id = NULL) {
-    $setting = $this->getSettingElement($form, $form_state);
-  }
-
-}
+class GCWebCDNFooterURL extends SettingBase {}
