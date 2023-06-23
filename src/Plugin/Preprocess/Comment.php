6796621406ecacdad1042f034afcf63c48e38f94
Joseph Olstad <20895-joseph.olstad@users.noreply.drupalcode.org>
2023-06-23 14:16:05 -0400

    Issue #3367309 by joseph.olstad: wxt_ext_blog - Firstname Lastname - Date

--- src/Plugin/Preprocess/Comment.php
+++ src/Plugin/Preprocess/Comment.php
@@ -19,6 +19,22 @@ class Comment extends PreprocessBase {
    */
   public function preprocessVariables(Variables $variables) {
     $variables['author'] = \Drupal::service('renderer')->render($elements);
+    $owner = $variables['comment']->getOwner();
+    if (empty($variables['author']) && $owner->id() === 0) {
+      $variables['author'] = $this->t('Anonymous');
+    }
+    if (empty($variables['author']) && $owner->isAuthenticated()) {
+      if ($owner->hasField('field_first_name') && $owner->hasField('field_last_name')) {
+        $first_name = $owner->get('field_first_name')->getString();
+        $last_name = $owner->get('field_last_name')->getString();
+        if (!empty($first_name) && !empty($last_name)) {
+          $variables['author'] = $first_name . ' ' . $last_name;
+        }
+      }
+    }
+    if (empty($variables['author'])) {
+      $variables['author'] = $variables['comment']->getAuthorName();
+    }
 
     // Getting the node creation time stamp from the comment object.
     $date = $variables['comment']->getCreatedTime();
